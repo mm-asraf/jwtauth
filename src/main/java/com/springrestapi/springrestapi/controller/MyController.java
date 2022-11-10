73ed0446e1c9e181a68364cde2c0d9e04ec225c2
mahboob asraf <mahboobasraf@gmail.com>
2022-11-10 17:56:45 +0530

    updated

--- src/main/java/com/springrestapi/springrestapi/controller/MyController.java
+++ src/main/java/com/springrestapi/springrestapi/controller/MyController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ import com.springrestapi.springrestapi.payload.UserResponse;
 import com.springrestapi.springrestapi.services.UserService;
 
 @RestController
+@CrossOrigin("*")
 public class MyController {
 	
 	@Autowired
